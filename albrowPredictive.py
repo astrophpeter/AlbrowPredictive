d604562392f08e2bb781d9982241f573dba5f71e
Peter <petermcgill94@gmail.com>
2019-10-03 23:50:22 +0100

    working code

--- albrowPredictive.py
+++ albrowPredictive.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.stats import uniform
-from scipy.optimize import minimize
+from scipy.optimize import differential_evolution
 
 class AlbrowPredictive:
 
@@ -17,37 +17,35 @@ class AlbrowPredictive:
         self.mag_min = np.min(mags)
         self.mag_max = np.max(mags)
     
-        #initialized the MAP values of the parameters
-        self.MAP_params = np.zeros(5)
-    
-        #initialize the MLE values of the parameter
-        self.MLE_params = np.zeros(5)
+        #initialized the MAP and MLE values of the parameters
+        self.map_params = np.zeros(5)
+        self.mle_params = np.zeros(5)
 
         #initial quess for the optimization
-        self.initial_params = np.array([20.0,10.0,5.0,0.8,np.mean(mag)])
-    
+        self.initial_params = np.array([2.0,5.0,7.0,0.8,np.mean(mags)])
+        self.initial_params_bounds = [(-1,10),(0.1,8.0),(-1,10),(0.0001,0.999),(self.mag_min,self.mag_max)] 
  
     def compute_log_prior(self,params):
         """Calcuales the the natural log of
         the prior p(params)  for a given set of parameters.
         """
-        ln_pr_ln_tE = params[0]
-        ln_pr_ln_A0 = params[1]
-        ln_pr_deltaT = params[2]
-        ln_pr_fbl = params[3]
-        ln_pr_mb = params[4]
+        ln_tE = params[0]
+        ln_A0 = params[1]
+        ln_deltaT = params[2]
+        fbl = params[3]
+        mb = params[4]
 
-        # Equation (16)
-        ln_pr_ln_tE = np.log(0.476) - (ln_tE-1.333)**2 / 0.330 
-        # Equation (15)
-        ln_pr_ln_A0 = np.log(0.660) - (-1.289*ln_A0)
-        # Equation (17)
-        ln_pr_deltaT = np.log(0.156) - (deltaT-1.432)**2 / 0.458
+        # Equation (16,15,17)
+        ln_pr_ln_tE = np.log(0.476) - ((ln_tE-1.333)**2 / 0.330) 
+        ln_pr_ln_A0 = np.log(0.660) - (1.289*ln_A0)
+        ln_pr_ln_deltaT = np.log(0.156) - ((ln_deltaT-1.432)**2 / 0.458)    
+     
         # Paper doesnt mention the prior used, but I assume it to be uniform
         ln_pr_fbl = uniform.logpdf(fbl,0.0,1.0)
+
         # Paper doesnr mention the prior used but I will asuumed it to be uniform
         ln_pr_mb = uniform.logpdf(mb,self.mag_min - 1.0, self.mag_max + 1.0)
-
+ 
    
         return ln_pr_fbl + ln_pr_ln_A0 + ln_pr_ln_deltaT + ln_pr_ln_tE + ln_pr_mb
 
@@ -57,8 +55,8 @@ class AlbrowPredictive:
         the likelihood p(data|params) for a given set of paramters
         """
         pred_mag  = self._pred_mag(params,self.times)
-        sigma_2 = sd_mags**2         
-        log_likelihood = -0.5*np.sum((pred_mag - mags)**2 / sigma_2+ np.log(sigma_2))
+        sigma_2 = self.sd_mags**2         
+        log_likelihood = -0.5*np.sum((pred_mag - self.mags)**2 / sigma_2+ np.log(sigma_2))
 
         return log_likelihood
 
@@ -69,58 +67,61 @@ class AlbrowPredictive:
         """Calculates the the natural log of the p(data,params)
         for the given parameters
         """
-        return self.compute_log_prior() + self.compute_log_likelihood() 
+        return self.compute_log_prior(params) + self.compute_log_likelihood(params) 
 
-    def neg_log_prob(self,paramas):
-        return -self.compute_log_prob()
+    def neg_log_prob(self,params):
+        return -self.compute_log_prob(params)
 
     def train_MAP(self):
         """Finds the Maximum A-prior estimate of the
         model parameters
         """
         
-        result = minimize(self.neg_log_prob,x0=self.initial_params)
-        self.MAP_params = result['x']
+        result = differential_evolution(self.neg_log_prob,
+                          bounds=self.initial_params_bounds)
+        self.map_params = result['x']
         print(result)
-
 
     def train_MLE(self):
         """Finds the maximum likelihood estimate of the
         model parameters
         """
 
-        result = minimize(self.neg_log_likelihood,x0=self.initial_params)
-        self.MLE_params = results['x']
+        result = differential_evolution(self.neg_log_likelihood,
+                         bounds=self.initial_params_bounds)
+        self.mle_params = result['x']
+
+        print(result)
 
     def predict_MAP(self,new_times):
         """Predict magnitudes at the times new times using the
         MAP paramters
         """
-        return self._pred_mag(self.MAP_params,new_times)
+        return self._pred_mag(self.map_params,new_times)
 
     def predict_MLE(self,new_times):
         """Predict magnitued at the times new times using
         the MLE paramters
         """
-        return self._pred_mag(self.MLE_params,new_times)
-
+        return self._pred_mag(self.mle_params,new_times)
+        
 
     def _pred_mag(self,params,times):
         """Computes the magnitude for the PSPL model at given times,
         and with model parameters.
         """
-        tE = np.exp(params[0])
-        A0 = np.exp(params[1])
-        deltaT = params[2]
+        tE = 10**params[0]
+        A0 = 10**params[1]
+        deltaT = 10**params[2]
         fbl = params[3]
         mb = params[4]
 
 
         u0 = np.sqrt((2*A0/np.sqrt(A0**2-1))-2)
-        u = np.sqrt(u0**2+((self.times-deltaT-self.alert_time)/tE)**2)
+        u = np.sqrt(u0**2+((times-deltaT-self.alert_time)/tE)**2)
         Amp = (u**2+2) / (u*np.sqrt(u**2+4))
 
-        pred_mag  = mb - 2.5*np.log10(Amp+fbl)
+        pred_mag  = mb - 2.5*np.log10(fbl*(Amp-1)+1)
 
         return pred_mag
 
@@ -130,6 +131,7 @@ class AlbrowPredictive:
         baseline.
         """
         
+        # Also not clear from the paper how to doe this,
         # use the first 10 data points in the light curve to determine the magnitude
         # baseline
 
@@ -139,17 +141,19 @@ class AlbrowPredictive:
         num_above = 0 
         i = 9
 
-        while num_above < 3 and i < len(self.times):
+        while num_above < 3 and i < len(self.times)-1:
             
             i += 1 
 
-            if mags[i] < mean_mag - std_mag:
+            if self.mags[i] < mean_mag - std_mag:
                 num_above += 1
             else:
                 num_above = 0.0
 
-           
-        return times[i] 
+        if len(self.times) - 1 == i:
+            print("Give me more training data, not alerted yet, this is probably going to fail")
+         
+        return self.times[i-1] 
 
 
 
--- tests/test_albrowPredictive.py
+++ tests/test_albrowPredictive.py
@@ -1,5 +1,32 @@
 import pytest
+import numpy as np
 
+from ..albrowPredictive import AlbrowPredictive
 
-def test_test():
-    assert 2 == 2
+def test_prior():
+    """Test if the specific fixed the prior evaluates
+    to the correct values
+    """
+
+    #dummy data
+    mags = np.linspace(1,20,50)
+    emags = np.random.random_sample(50)
+    times = np.linspace(1,200,50)
+    model = AlbrowPredictive(times,mags,emags)
+    
+    ln_tE = 1.333
+    ln_A0 = 3.0
+    ln_deltaT = 1.432
+    fbl = 0.5
+    mb = 10.0
+
+    params = np.array([ln_tE,ln_A0,ln_deltaT,fbl,mb])
+    ln_prior = model.compute_log_prior(params)
+    
+    ans = (np.log(0.660) - 1.289*3 
+           + np.log(0.476) 
+           + np.log(0.156) 
+           + np.log(1.0) 
+           + np.log(1.0/(21.0-0.0)))
+  
+    assert ln_prior == ans
