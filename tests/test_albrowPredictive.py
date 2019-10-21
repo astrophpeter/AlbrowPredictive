4379b146295d9f82f11ef6a65931d8bf5818b0ef
Fran Bartolic <fb90@st-andrews.ac.uk>
2019-10-21 17:58:43 +0100

    Fixed test

--- tests/test_albrowPredictive.py
+++ tests/test_albrowPredictive.py
@@ -13,17 +13,19 @@ def test_prior():
     emags = np.random.random_sample(50)
     times = np.linspace(1,200,50)
     model = AlbrowPredictive(times,mags,emags)
+
+    log10e = np.log10(np.exp(1))
     
-    ln_tE = 1.333
-    ln_A0 = 3.0
-    ln_deltaT = 1.432
+    ln_tE = 1.333/log10e
+    ln_A0 = 3.0/log10e
+    ln_deltaT = 1.432/log10e
     fbl = 0.5
     mb = 10.0
 
     params = np.array([ln_tE,ln_A0,ln_deltaT,fbl,mb])
     ln_prior = model.compute_log_prior(params)
-    
-    ans = (np.log(0.660) - 1.289*3 
+   
+    ans = (np.log(0.660) - 1.289*3 + 3*np.log(log10e)
            + np.log(0.476) 
            + np.log(0.156) 
            + np.log(1.0) 
